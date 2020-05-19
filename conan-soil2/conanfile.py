2ba94106e7c02d04fcca671e09f7d2d38a7d8e46
rmanciaux <romain.manciaux@gmail.com>
2020-05-19 20:13:46 +0200

    refactor(conan-soil2): improve the CMakeLists

--- conan-soil2/conanfile.py
+++ conan-soil2/conanfile.py
@@ -21,8 +21,10 @@ class Soil2Conan(ConanFile):
         "fPIC": True
     }
     exports = "LICENSE.md"
-    exports_sources = os.path.join("patches", "CMakeLists.txt")
-    short_paths=True
+    exports_sources = [
+        os.path.join("patches", "CMakeLists.txt"),
+        os.path.join("patches", "SOIL2Config.cmake.in")
+    ]
     
     _source_folder = "{0}-{1}_sources".format(name, version)
     _build_folder = "{0}-{1}_build".format(name, version)
@@ -38,7 +40,8 @@ class Soil2Conan(ConanFile):
     def source(self):
         tools.get("{0}/archive/release-{1}.tar.gz".format(self.homepage, self.version), sha256="104a2de5bb74b58b7b7cda7592b174d9aa0585eeb73d0bec4901f419321358bc")
         os.rename("SOIL2-release-{0}".format(self.version), self._source_folder)
-        shutil.copy(self.exports_sources, self._source_folder)
+        for export_source in self.exports_sources:
+            shutil.copy(export_source, self._source_folder)
 
     def build(self):
         cmake = CMake(self)
@@ -55,9 +58,11 @@ class Soil2Conan(ConanFile):
         self.cpp_info.libs = tools.collect_libs(self)
         self.cpp_info.names["cmake_find_package"] = "SOIL2"
         self.cpp_info.names["cmake_find_package_multi"] = "SOIL2"
+        # Set the package folder as CMAKE_PREFIX_PATH to find SOIL2Config.cmake.
+        self.env_info.CMAKE_PREFIX_PATH.append(self.package_folder)
         if tools.os_info.is_linux:
             self.cpp_info.libs.append('GL')
         elif tools.os_info.is_macos:
-            self.cpp_info.sharedlinkflags.append("-framework OpenGL -framework CoreFoundation")
+            self.cpp_info.frameworks.extend(["OpenGL", "CoreFoundation"])
         elif tools.os_info.is_windows:
             self.cpp_info.libs.append('opengl32')
