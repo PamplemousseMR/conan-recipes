664df4930eb53f06f5a0e7d121d0ec7357221d83
rmanciaux <romain.manciaux@gmail.com>
2020-05-23 16:01:28 +0200

    refactor(conan-glm): improve the recipe

--- conan-glm/conanfile.py
+++ conan-glm/conanfile.py
@@ -1,18 +1,16 @@
-from conans import ConanFile, tools
 import os
+from conans import ConanFile, tools
 from distutils.dir_util import copy_tree
+
 
 class GlmConan(ConanFile):
     name = "glm"
-    version = "0.9.9.7"
+    version = "0.9.9.8"
     description = "OpenGL Mathematics (GLM)"
     homepage = "https://github.com/g-truc/glm"
     url = "https://github.com/PamplemousseMR/conan-recipes"
     license = "MIT"
-    author = "MANCIAUX Romain (https://github.com/PamplemousseMR)"
-    generators = "cmake"
-    exports = "LICENSE.md"
-    short_paths=True
+    short_paths = True
     no_copy_source = True
 
     _source_folder = "{0}-{1}_sources".format(name, version)
@@ -21,12 +19,19 @@ class GlmConan(ConanFile):
         self.info.header_only()
 
     def source(self):
-        tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version), sha256="2ec9e33a80b548892af64fbd84a947f93f0e725423b1b7bec600f808057a8239")
+        tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version),
+                  sha256="7d508ab72cb5d43227a3711420f06ff99b0a0cb63ee2f93631b162bfe1fe9592")
         os.rename("{0}-{1}".format(self.name, self.version), self._source_folder)
 
     def package(self):
-        for export in self.exports:
-            self.copy(export, keep_path=False)
+        # Copying the license file.
+        self.copy("copying.txt", src=self._source_folder, dst="licenses", keep_path=False)
         copy_tree(
-            os.path.join(self.source_folder, self._source_folder, self.name),
-            os.path.join(self.package_folder, "include", self.name))
+            os.path.join(self.source_folder, self._source_folder, "glm"),
+            os.path.join(self.package_folder, "include", "glm"))
+        os.remove(os.path.join(self.package_folder, "include", "glm", "CMakeLists.txt"))
+
+    def package_info(self):
+        # Set the name of conan auto generated FindGLM.cmake.
+        self.cpp_info.names["cmake_find_package"] = "GLM"
+        self.cpp_info.names["cmake_find_package_multi"] = "GLM"
