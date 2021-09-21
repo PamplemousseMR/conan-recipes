dc9663cd71a89d1705d0248825dd5c70244b79ac
Romain MANCIAUX <romain.manciaux@gmail.com>
2021-09-21 17:10:06 +0200

    feat(opencv): add more version

--- conan-opencv/conanfile.py
+++ conan-opencv/conanfile.py
@@ -23,13 +23,13 @@ class OpenCVConan(ConanFile):
     default_options = {
         "shared": True,
         "fPIC": True,
-        "with_png": True,
+        "with_png": False,
         "parallel": False,
         "with_cuda": False,
         "with_cublas": False,
-        "with_protobuf": True,
+        "with_protobuf": False,
         "with_cudnn": False,
-        "contrib": True
+        "contrib": False
     }
     short_paths = True
 
