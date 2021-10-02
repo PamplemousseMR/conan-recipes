11d8c6455d078a0fef60e943620a1d2db9d9a2fa
Romain MANCIAUX <romain.manciaux@gmail.com>
2021-10-02 21:19:50 +0200

    refactor(opencv): improve package

--- conan-opencv/conanfile.py
+++ conan-opencv/conanfile.py
@@ -8,6 +8,8 @@ class OpenCVConan(ConanFile):
     homepage = "https://github.com/opencv/opencv"
     url = "https://github.com/PamplemousseMR/conan-recipes"
     license = "Apache"
+    author = "MANCIAUX Romain (https://github.com/PamplemousseMR)"
+    generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -31,6 +33,7 @@ class OpenCVConan(ConanFile):
         "with_cudnn": False,
         "contrib": False
     }
+    exports_sources = "CMakeLists.txt"
     short_paths = True
 
     _source_folder = "{0}_sources".format(name)
@@ -163,7 +166,7 @@ class OpenCVConan(ConanFile):
         if self.options.with_protobuf:
             cmake.definitions["PROTOBUF_UPDATE_FILES"] = True
 
-        cmake.configure(source_folder=self._source_folder, build_folder=self._build_folder)
+        cmake.configure(build_folder=self._build_folder)
         cmake.build()
         cmake.install()
 
@@ -172,15 +175,18 @@ class OpenCVConan(ConanFile):
         self.copy("LICENSE", src=self._source_folder, dst="licenses", ignore_case=True, keep_path=False)
         self.copy(pattern="*.pdb", dst="bin", keep_path=False)
 
+        tools.rmdir(os.path.join(self.package_folder, 'etc'))
+
     def package_info(self):
+        if tools.os_info.is_windows:
+            install_path = os.path.join("x64" if self.settings.arch == "x86_64" else "x32", "vc" + str(self.settings.compiler.version))
+            self.cpp_info.libdirs.append(os.path.join(install_path, "lib"))
+            self.cpp_info.bindirs.append(os.path.join(install_path, "bin"))
+
         self.cpp_info.libs = tools.collect_libs(self)
 
         # Set the name of conan auto generated FindOpenCV.cmake.
-        self.cpp_info.names["cmake_find_package"] = "OpenCV"
-        self.cpp_info.names["cmake_find_package_multi"] = "OpenCV"
-
-        # Set the name of conan auto generated opencv.pc.
-        self.cpp_info.names["pkg_config"] = "OpenCV"
+        self.cpp_info.name = "OpenCV"
 
         # Set the package folder as CMAKE_PREFIX_PATH to find OpenCVConfig.cmake.
         self.env_info.CMAKE_PREFIX_PATH.append(self.package_folder)
